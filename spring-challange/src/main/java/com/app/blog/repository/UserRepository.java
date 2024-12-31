45e46c1d044862db686d089b194d46c2fc6172ca
pattabhik <58624314+pattabhik@users.noreply.github.com>
2024-12-31 13:09:39 +0000

    Update UserRepository.java

--- spring-challange/src/main/java/com/app/blog/repository/UserRepository.java
+++ spring-challange/src/main/java/com/app/blog/repository/UserRepository.java
@@ -18,4 +18,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<Users,Integer> {
 
+    Optional<Users> findByEmailAndPassword(String email, String password);
 }
