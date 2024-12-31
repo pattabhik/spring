80543ed8472acc0943a9071085e360083c5d28b6
pattabhik <58624314+pattabhik@users.noreply.github.com>
2024-12-31 13:08:12 +0000

    Update UserController.java
    made the changes according to junit

--- spring-challange/src/main/java/com/app/blog/controllers/UserController.java
+++ spring-challange/src/main/java/com/app/blog/controllers/UserController.java
@@ -5,12 +5,14 @@ import com.app.blog.dto.RegisterUserDTO;
 import com.app.blog.models.Users;
 import com.app.blog.repository.UserRepository;
 import com.app.blog.util.EntitiyHawk;
+import com.app.blog.util.JWTUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -27,17 +29,33 @@ public class UserController extends EntitiyHawk {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    JWTUtils jwtUtils;
+
     @PostMapping("/register")
-    public void register(@RequestBody RegisterUserDTO registerUserDTO) {
-        Users user = new Users();
-        user.setUserName(registerUserDTO.getName());
-        user.setPassword(registerUserDTO.getPassword());
-        user.setEmail(registerUserDTO.getEmail());
-        userRepository.save(user);
+    public ResponseEntity register(@Valid @RequestBody RegisterUserDTO registerUserDTO, BindingResult result) {
+        Object response = "User Registered";
+        if (result.hasErrors()) {
+            response = result.getFieldError().getField() + " " + result.getFieldError().getDefaultMessage();
+        }else {
+            Users user = new Users();
+            user.setUserName(registerUserDTO.getName());
+            user.setPassword(registerUserDTO.getPassword());
+            user.setEmail(registerUserDTO.getEmail());
+            userRepository.save(user);
+        }
+        return genericSuccess(response);
     }
 
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody LoginDto loginDto) {
-        return genericSuccess(loginDto);
+    public ResponseEntity login(@Valid @RequestBody LoginDto loginDto, BindingResult result) {
+        Object response = null;
+        Optional<Users> user = userRepository.findByEmailAndPassword(loginDto.getEmail(), loginDto.getPassword());
+        if (result.hasErrors() || user==null || !user.isPresent()) {
+            response = "Invalid Username or Password";
+        }else {
+            response = jwtUtils.CreateJWTToken(user.orElse(null));
+        }
+        return genericSuccess(response);
     }
 }
